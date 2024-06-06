5fbfb198a6d47f39deb307f71a8fd743015316db
Artur-MacBook-Air <artur@example.com>
2024-06-06 20:02:00 +0300

    Editing, Updating, and Deleting a Resource

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -3,9 +3,13 @@
         Job
     </x-slot:heading>
     
-    <h2 class="font-bold text-lg">{{ $job['title'] }}</h2>
+    <h2 class="font-bold text-lg">{{ $job->title }}</h2>
 
     <p>
-        This job pays {{ $job['salary'] }} per year
+        This job pays {{ $job->salary }} per year
     </p>  
+
+    <div class="mt-6">
+        <x-button href="/jobs/{{ $job->id }}/edit">Edit job</x-button>
+    </div>
 </x-layout>
